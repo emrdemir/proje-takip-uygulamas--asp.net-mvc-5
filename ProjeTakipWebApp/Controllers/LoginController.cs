9f502721dc8334d85fa14438a3cbf76e837e0104
emredemir <emre1demir6@gmail.com>
2023-08-28 17:15:53 +0300

    login-ekranı-eklenmesi

---
+++ ProjeTakipWebApp/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using ProjeTakipWebApp.Models;
+using ProjeTakipWebApp.Models.Personel;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ProjeTakipWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private ProjeTakipDBContext db = new ProjeTakipDBContext();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(PersonelBilgileri admin)
+        {
+            var bilgiler = db.PersonelBilgileris.FirstOrDefault(x => x.AdSoyad == admin.AdSoyad && x.Sifre == admin.Sifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.AdSoyad, false);
+                Session["Kullanici"] = bilgiler.AdSoyad.ToString();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session["Kullanici"] = null;
+            Session.Abandon();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
